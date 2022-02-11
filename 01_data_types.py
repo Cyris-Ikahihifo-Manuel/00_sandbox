e06b5aa7c9033adb3d9defd0971b756933a6d4e3
ikahih.cyris <ikahih.cyris@awatapu.school.nz>
2022-02-11 14:00:32 +1300

    data types

--- 01_data_types.py
+++ 01_data_types.py
@@ -16,22 +16,43 @@ def data_types(question, condition, error):
             print("")
 
 
-# data types program running
+# age question function
 
-response = ["", ""]
 
-questions = [data_types(input("What's your name?").strip(), "", "You must enter something other than pressing enter key"),
-             data_types(input("What's your favorite animal?").strip(), "", "You must enter something other than pressing enter key")]
-
-while True:
-    try:
-        response[1] = int(input())
-        print("")
-        if response[1] >= 0:
-            return response
-        else:
+def age():
+    while True:
+        try:
+            answer = int(input("How old are you?"))
+            print("")
+            if answer > 0:
+                return answer
+            else:
+                print("You must enter a valid age")
+                print("")
+        except ValueError:
             print("You must enter a valid age")
             print("")
-    except ValueError:
-        print("You must enter a valid age")
-        print("")
+
+
+# whats PIs digit function
+
+
+def whats_pi():
+    while True:
+        try:
+            answer = int(input())
+            print("What are the first three decimals of pi?")
+            return answer
+        except ValueError:
+            print("You must enter a number")
+            print("")
+
+
+# data types program running
+
+
+questions = [data_types(input("What's your name?").strip(), "", "You must enter something other than pressing enter key"), age(),
+             data_types(input("What's your favorite animal?").strip(), "", "You must enter something other than pressing enter key"),
+             whats_pi()]
+
+print(questions)
