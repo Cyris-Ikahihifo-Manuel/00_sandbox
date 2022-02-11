92dc0959186ff09c698c42f7a0e9c27d27a4c632
ikahih.cyris <ikahih.cyris@awatapu.school.nz>
2022-02-11 13:35:51 +1300

    data types

--- 01_data_types.py
+++ 01_data_types.py
@@ -20,5 +20,4 @@ def data_types(question, condition, error):
 
 questions = [data_types(input("What's your name?").strip(), "", "You must enter something other than pressing enter key"),
              data_types(int(input("How old are you?")), "", "You must enter your age in numbers"),
-             data_types(input("What's your favorite animal?").strip(), "", "You must enter something other than pressing enter key"),
-             data_types(float(input("What are the first four digits of PI?")), "", "The answer is a number followed by decimals")]
+             data_types(input("What's your favorite animal?").strip(), "", "You must enter something other than pressing enter key")]
